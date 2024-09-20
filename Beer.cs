1caaedb20cb5d7e9e3bd1e0d865f81cb0484df00
Johan <147413295+jkrangh@users.noreply.github.com>
2024-09-20 15:35:52 +0200

    Add project files.

---
+++ Beer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapApp
+{
+
+    public class Beer
+    {
+        public int id { get; set; }
+        public string uid { get; set; }
+        public string brand { get; set; }
+        public string name { get; set; }
+        public string style { get; set; }
+        public string hop { get; set; }
+        public string yeast { get; set; }
+        public string malts { get; set; }
+        public string ibu { get; set; }
+        public string alcohol { get; set; }
+        public string blg { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using System.Text.Json;
+using TapApp;
+
+Beer beer = new Beer();
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("https://random-data-api.com/");
+client.DefaultRequestHeaders.Clear();
+
+HttpResponseMessage response = await client.
+    GetAsync("api/v2/beers");
+if (response.IsSuccessStatusCode)
+{
+    var result = response.Content.ReadAsStringAsync().Result;
+    beer = JsonSerializer.Deserialize<Beer>(result);
+}
+
+    Type type = beer.GetType();
+    PropertyInfo[] properties = type.GetProperties();
+
+    foreach (PropertyInfo property in properties)
+    {
+        string propertyName = property.Name;
+        object propertyValue = property.GetValue(beer);
+
+        Console.WriteLine($"{propertyName}: {propertyValue}");
+    }
+
+
